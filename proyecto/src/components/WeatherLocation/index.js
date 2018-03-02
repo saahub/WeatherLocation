d36aa9f1ec86fe4c1afd867fff071a0f8d747ddb
lpalominos <lorena.palominos@gmail.com>
2018-03-02 17:55:12 -0300

    actualizado

--- proyecto/src/components/WeatherLocation/index.js
+++ proyecto/src/components/WeatherLocation/index.js
@@ -3,9 +3,9 @@ import Location from './Location';
 import WeatherData from './WeatherData';
 import {CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY} from './../../constant/weathers';
 
-const api_key = 631deffc7b3dc563f3a19aea3aa586d2;
+const api_key = "631deffc7b3dc563f3a19aea3aa586d2";
 const location = 'Santiago, scl';
-const api_weather = `api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
+const api_weather = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}&units=metric`;
 
 const data1 = {
   temperature: 20,
@@ -31,10 +31,36 @@ class WeatherLocation extends Component {
       data: data1
     }
   }
+
+  getWeatherState = weather => {
+    return SUN;
+  }
+
+  getData = (weather_data) => {
+    const {humidity, temp} = weather_data.main;
+    const {speed} = weather_data.wind;
+    const weatherState = this.getWeatherState(this.weather);
+
+    const data = {
+      humidity,
+      temperature: temp,
+      weatherState,
+      wind: `${speed} m/s`
+    }
+    return data;
+  }
+
   handleUpdateClick = () => {
-    this.setState({
-      data: data2
+    fetch(api_weather).then(data => {
+      console.log(data);
+      return data.json();
+    }).then(weather_data => {
+      const data = this.getData(weather_data);
+      this.setState({data})
+      console.log(weather_data);
     })
+
+
     console.log('actualizado');
   }
   render = () => {
